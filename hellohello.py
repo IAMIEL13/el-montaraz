d297726c017a14991e1957a09e982de6eaacc47e
IAMIEL13 <a.amielalmada@icea.edu.ar>
2024-10-01 19:29:01 +0000

    2

--- hellohello.py
+++ hellohello.py
@@ -1,1 +1,13 @@
-g
+("Braian" "Amiel" "Almada")
+from tokenize import String
+
+
+String-("Hola")
+numeros=(23.3+3)-2+5
+booleanos=(True)
+booleanos=(False)
+print(23)("vivalavida")
+it= (True)
+print(2+2)
+(booleanos)=(False or True)    
+    
