eef78fac5d52229b82e825f903f63ac2c357e0dd
IAMIEL13 <a.amielalmada@icea.edu.ar>
2024-10-08 18:51:19 +0000

    xD

--- hellohello.py
+++ hellohello.py
@@ -1,13 +1,12 @@
-("Braian" "Amiel" "Almada")
-from tokenize import String
-
-
-String-("Hola")
-numeros=(23.3+3)-2+5
+lista=("braian", "amiel", "Almada")
+Nombre=lista(1)
+Apellido=lista(2)
+String=("hello")
+Numeros=(2.3.3+3)=-2+5
+Numeros=293
 booleanos=(True)
-booleanos=(False)
-print(23)("vivalavida")
-it= (True)
-print(2+2)
-(booleanos)=(False or True)    
-    
+booleanos=(False,True)
+if booleanos(0):
+    print(23)
+    it="pennywise"
+    ("booleanos")=(False or True)
